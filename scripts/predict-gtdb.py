5fbe59a124b5ec62fb1924e85b2382555eaf9388
Philippa Richter <pips.richter@gmail.com>
2024-08-12 22:15:54 -0700

    ok

--- scripts/predict-gtdb.py
+++ scripts/predict-gtdb.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     results = []
 
     embedding_file_names = os.listdir(EMBEDDINGS_PATH) # Filenames are the RS_ or GB_ prefix followed by the genome ID. 
-    genome_ids = [re.search('GC[AF]_\d{9}\.\d{1}', self.file_name).group(0) for file in embedding_file_names]
+    genome_ids = [re.search('GC[AF]_\d{9}\.\d{1}', file_name).group(0) for file_name in embedding_file_names]
 
     for embedding_file_name, genome_id in tqdm(zip(embedding_file_names, genome_ids), desc='Processing genomes...'):
         seld_copy_num, sela_copy_num, selb_copy_num = get_copy_numbers()
