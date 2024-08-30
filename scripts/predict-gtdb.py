f07d481e407a45ceb04b9bc4c808c96f846c6664
Philippa Richter <pips.richter@gmail.com>
2024-08-30 13:15:02 -0700

    updating gtdb stuff

--- scripts/predict-gtdb.py
+++ scripts/predict-gtdb.py
@@ -36,7 +36,7 @@ SELD_KO = 'K01008'
 SELA_KO = 'K01042' 
 SELB_KO = 'K03833' 
 
-# sbatch --mem 100GB --time 100:00:00 --wrap "python predict-gtdb --results-dir /central/groups/fischergroup/prichter/selenobot/results/ --model model_epochs_1000_lr_e7.pkl"
+# sbatch --mem 100GB --time 100:00:00 --wrap "python predict-gtdb.py --results-dir /central/groups/fischergroup/prichter/selenobot/results/"
 def get_copy_numbers(output_path:str=None):
     '''Take what is probably a faster approach to finding copy numbers, which is to grab every annotation which matches one of the 
     selenoprotein genes, and then group the result by genome.'''
@@ -81,16 +81,22 @@ def get_genome_data(genome_ids:List[str], batch_size=50, output_path:str=None):
         query = Query('metadata')
         query.equal_to('genome_id', batch)
         genome_data_df.append(query.get())
-    
     genome_data_df = pd.concat(genome_data_df).set_index('genome_id')
     genome_data_df.to_csv(output_path)
     print(f"get_genome_data: Genome data written to {output_path}")
 
 
+def get_annotation_data(gene_ids:List[str], batch_size=100, output_path:str=None):
+    annotation_data_df = []
+    for batch in tqdm([gene_ids[i * batch_size:(i + 1) * batch_size] for i in range(len(gene_ids) // batch_size + 1)], desc='get_annotation_data: Fetching annotations...'):
+        query = Query('annotations_kegg')
+        annotation_data_df.append(query.get()[['ko', 'gene_id']])
+    annotation_data_df = pd.concat(annotation_data_df)
+    annotation_data_df.set_index('gene_id').to_csv(os.path.join(output_path))
+    print(f"get_annotation_data: Annotation data written to {output_path}")
+
+
 def get_gene_data(gene_ids:List[str], batch_size=100, output_path:str=None):
-
-    results_dir, _ = os.path.split(output_path)
-
     # NOTE: As long as the batch size is less than 1000 (which I think is the default page size), should not need to paginate at all. 
     gene_data_df = []
     for batch in tqdm([gene_ids[i * batch_size:(i + 1) * batch_size] for i in range(len(gene_ids) // batch_size + 1)], desc='get_gene_data: Fetching gene data...'):
@@ -98,18 +104,7 @@ def get_gene_data(gene_ids:List[str], batch_size=100, output_path:str=None):
         gene_data_df.append(query.get())
     gene_data_df = pd.concat(gene_data_df)
     gene_data_df.set_index('gene_id').to_csv(output_path)
-    
-
-    annotations_df = []
-    for batch in tqdm([gene_ids[i * batch_size:(i + 1) * batch_size] for i in range(len(gene_ids) // batch_size + 1)], desc='get_gene_data: Fetching annotations...'):
-        query = Query('annotations')
-        annotations_df.append(query.get()[['ko', 'gene_id']])
-    annotations_df = pd.concat(annotations_df)
-    annotations_df.set_index('gene_id').to_csv(os.path.join(results_dir, 'gtdb_annotations.csv'))
-    
-    gene_data_df = gene_data_df.merge(annotations_df, left_on='gene_id', right_on='gene_id', how='left').set_index('gene_id')
-    gene_data_df.to_csv(output_path)
-    print(f"get_gene_data: Gene data written to {output_path}")
+    print(f"get_gene_data: Gene and annotations data written to {output_path}")
 
 
 def get_predictions(model:str, embeddings_dir:str=EMBEDDINGS_DIR, models_dir:str=MODELS_DIR, output_path:str=None):
@@ -144,7 +139,7 @@ def get_predictions(model:str, embeddings_dir:str=EMBEDDINGS_DIR, models_dir:str
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default='model_epochs_100_lr_e8.pkl', type=str)
+    parser.add_argument('--model', default='model_epochs_2000_lr_e8.pkl', type=str)
     # parser.add_argument('--output-path', default=os.path.join(args.results_dir, 'gtdb_results.csv'), type=str)
     parser.add_argument('--results-dir', default='/home/prichter/Documents/selenobot/results/epochs_2000_lr_e8')
 
@@ -157,6 +152,10 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join(args.results_dir, 'gtdb_gene_data.csv')):
         get_gene_data(predictions_df.gene_id.values, output_path=os.path.join(args.results_dir, 'gtdb_gene_data.csv'))   
     gene_data_df = pd.read_csv(os.path.join(args.results_dir, 'gtdb_gene_data.csv'), dtype={'partial':str})
+    
+    if not os.path.exists(os.path.join(args.results_dir, 'gtdb_annotation_data.csv')):
+        get_annotation_data(predictions_df.gene_id.values, output_path=os.path.join(args.results_dir, 'gtdb_annotation_data.csv'))   
+    annotation_data_df = pd.read_csv(os.path.join(args.results_dir, 'gtdb_annotation_data.csv'), dtype={'partial':str})
 
     if not os.path.exists(os.path.join(args.results_dir, 'gtdb_genome_data.csv')):
         get_genome_data(gene_data_df.genome_id.unique(), output_path=os.path.join(args.results_dir, 'gtdb_genome_data.csv'))
@@ -169,6 +168,7 @@ if __name__ == '__main__':
 
 
     results_df = predictions_df.merge(gene_data_df, how='left', left_on='gene_id', right_on='gene_id')
+    results_df = predictions_df.merge(annotation_data_df, how='left', left_on='gene_id', right_on='gene_id')
     results_df = results_df.merge(copy_nums_df, how='left', left_on='genome_id', right_on='genome_id')
     results_df = results_df.merge(genome_data_df, how='left', left_on='genome_id', right_on='genome_id')
 
