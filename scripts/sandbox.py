7e31e9d45aadcfa993d545ea20e576c1d4015a6b
Philippa Richter <pips.richter@gmail.com>
2024-09-19 13:16:15 -0700

    okie

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -1,15 +1,9 @@
 import pandas as pd
-import h5py
 import os
 import numpy as np
 from selenobot.datasets import Dataset, get_dataloader
-import matplotlib.pyplot as plt
 from selenobot.classifiers import *
-import scipy.stats
 from selenobot.files import *
-from sklearn.metrics import balanced_accuracy_score
-from scipy.stats import kstest, mannwhitneyu
-from fabapi import Query
 
 # Define some important directories...
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
