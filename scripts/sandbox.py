b6b8a4e335bf9826949c44f75cba9dacb997ae70
Philippa Richter <pips.richter@gmail.com>
2024-09-23 14:34:06 -0700

    okie

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -4,6 +4,7 @@ import numpy as np
 from selenobot.datasets import Dataset, get_dataloader
 from selenobot.classifiers import *
 from selenobot.files import *
+from selenobot.embedders import embed
 
 # Define some important directories...
 ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
@@ -12,6 +13,8 @@ MODELS_DIR = os.path.join(ROOT_DIR, 'models')
 DATA_DIR = os.path.join(ROOT_DIR, 'data') # Get the path where results are stored. 
 SCRIPTS_DIR = os.path.join(ROOT_DIR, 'scripts') # Get the path where results are stored.
 
-f = NcbiXmlFile(os.path.join(DATA_DIR, 'uniprot_sprot.xml'))
-f.dataframe().to_csv(os.path.join(DATA_DIR, 'uniprot_sprot.csv'))
+# f = NcbiXmlFile(os.path.join(DATA_DIR, 'uniprot_sprot.xml'))
+# f.dataframe().to_csv(os.path.join(DATA_DIR, 'uniprot_sprot.csv'))
 
+df = pd.read_csv(os.path.join(DATA_DIR, 'uniprot_truncated.csv'))
+embed(df, os.path.join(DATA_DIR, 'uniprot_truncated.h5'))
