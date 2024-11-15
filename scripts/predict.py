aeb258f82f72fbba40bfe7fa1847444d9d99e1e8
Philippa Richter <pips.richter@gmail.com>
2024-11-14 21:02:39 -0800

    updating predict

--- scripts/predict.py
+++ scripts/predict.py
@@ -5,23 +5,37 @@ import argparse
 import os
 
 
-# Define some important directories...
-ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
-RESULTS_DIR = os.path.join(ROOT_DIR, 'results') # Get the path where results are stored.
-MODELS_DIR = os.path.join(ROOT_DIR, 'models')
-DATA_DIR = os.path.join(ROOT_DIR, 'data') # Get the path where results are stored. 
-SCRIPTS_DIR = os.path.join(ROOT_DIR, 'scripts') # Get the path where results are stored.
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--feature-type', default='plm', type=str, help='The type of embedding to load from the Dataset.')
-    parser.add_argument('--input-path', help='The path to the embeddings for which to generate predictions.')
-    parser.add_argument('--model-name', default='model_epochs_1000_lr_e8.pkl', help='The name of the model to load.')
+    parser.add_argument('--feature-type', default='plm', type=str)
+    parser.add_argument('--input-path', type=str)
+    parser.add_argument('--n-classes', default=2)
+    parser.add_argument('--models-dir', default='../models', type=str)
+    parser.add_argument('--results-dir', default='../results', type=str)
 
     args = parser.parse_args()
 
-    dataset = Dataset.from_hdf(args)
-    
+    model_name = f'ternary_model_{args.feature_type}.pkl' if (args.n_classes == 3) else f'binary_model_{args.feature_type}.pkl' 
+    model_path = os.path.join(args.model_dir, model_name)
+
+    if args.n_classes == 2:
+        dataset = BinaryDataset.from_hdf(args.input_path, feature_type=args.feature_type)
+        model = BinaryClassifier.load(model_path)
+    elif args.output_dim == 3:
+        dataset = TernaryDataset.from_hdf(args.input_path, feature_type=args.feature_type)
+        model = TernaryClassifier.load(model_path)
+
+    output_file_name, _ = os.path.splitext(os.path.basename(args.input_path))
+    output_file_name = output_file_name + '_predictions.csv'
+    output_path = os.path.join(args.results_dir, output_file_name)
+
+    results = dataset.metadata
+    results['prediction'] = model.predict(dataset)
+    results.to_csv(output_path)
+
+    print(f'Predictions written to {output_path}')
+
 
