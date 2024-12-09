9184ce8ac1720bd463bf32a0d10056e19de8f18d
Philippa Richter <pips.richter@gmail.com>
2024-12-08 22:36:32 -0800

    debug

--- selenobot/cdhit.py
+++ selenobot/cdhit.py
@@ -72,7 +72,7 @@ class CdHit():
 
         clstr_path = self._run(self.cluster_output_path, c=self.c_cluster, overwrite=overwrite) # Run CD-HIT and get the path of the output file. 
         clstr_df = ClstrFile(clstr_path).to_df(reps_only=False) # Load in the cluster file and convert to a DataFrame. 
-        df = clstr_df.df.copy().merge(self.df, left_index=True, right_index=True, how='inner')
+        df = clstr_df.merge(self.df, left_index=True, right_index=True, how='inner')
         print(df[df.seq.isnull()])
         # The DataFrame should now contain the clustering information. 
         # df should now only contain the representative sequences. Store in the object.
