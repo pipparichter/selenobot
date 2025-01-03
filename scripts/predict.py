7eb861d3cb412b215c39571e5ca485bf33cd8aa2
Philippa Richter <pips.richter@gmail.com>
2025-01-03 11:02:03 -0800

    why is this so weird!

--- scripts/predict.py
+++ scripts/predict.py
@@ -10,30 +10,31 @@ import os
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--feature-type', default='plm', type=str)
+    parser.add_argument('--type', default='plm', type=str)
     parser.add_argument('--input-path', type=str)
     parser.add_argument('--n-classes', default=2, type=int, choices=[2, 3])
     parser.add_argument('--models-dir', default='../models', type=str)
-    parser.add_argument('--results-dir', default='../data/validation', type=str)
+    parser.add_argument('--results-dir', default='../data/results/', type=str)
+    parser.add_argument('--add-metadata', action='store_true')
 
     args = parser.parse_args()
 
-    model_name = f'ternary_model_{args.feature_type}.pkl' if (args.n_classes == 3) else f'binary_model_{args.feature_type}.pkl' 
+    model_name = f'ternary_model_{args.type}.pkl' if (args.n_classes == 3) else f'binary_model_{args.type}.pkl' 
     model_path = os.path.join(args.models_dir, model_name)
 
     model = Classifier.load(model_path)
     
-    dataset = Dataset.from_hdf(args.input_path, feature_type=args.feature_type, n_classes=args.n_classes)
+    dataset = Dataset.from_hdf(args.input_path, feature_type=args.type, n_classes=args.n_classes)
 
-    output_file_name, _ = os.path.splitext(os.path.basename(args.input_path))
-    output_file_name = output_file_name + f"_predictions_{model_name.replace('.pkl', '')}.csv"
-    output_path = os.path.join(args.results_dir, output_file_name)
+    results_file_name, _ = os.path.splitext(os.path.basename(args.input_path))
+    results_file_name = 'predict_' + results_file_name + f"_{model_name.replace('.pkl', '')}.csv"
+    results_path = os.path.join(args.results_dir, results_file_name)
 
-    results = dataset.metadata
-    predictions = model.predict(dataset)
-    results = results.merge(predictions, left_index=True, right_index=True)
-    results.to_csv(output_path)
+    results_df = model.predict(dataset)
+    if args.add_metadata:
+        results_df = results_df.merge(dataset.metadata, left_index=True, right_index=True)
+    results_df.to_csv(results_path)
 
-    print(f'Predictions written to {output_path}')
+    print(f'Predictions written to {results_path}')
 
 
