43bb552ba41e6e08d3fe39c814ec0db7845663bc
Philippa Richter <pips.richter@gmail.com>
2025-01-23 15:45:40 -0800

    weirdness with esm

--- scripts/bash/embed.sh
+++ scripts/bash/embed.sh
@@ -7,9 +7,11 @@ PARTITION=gpu
 MEM=300GB
 TIME=10:00:00
 
-directory="../data/model_organisms/"
+# directory="../data/model_organisms/"
+directory="../data/"
 feature_types="plm_pt5 plm_esm aa_1mer len"
-file_names=("gtdb_ecol_metadata.csv" "gtdb_bsub_metadata.csv" "gtdb_mtub_metadata.csv")
+# file_names=("gtdb_ecol_metadata.csv" "gtdb_bsub_metadata.csv" "gtdb_mtub_metadata.csv")
+file_names=("test_metadata.csv")
 
 for file_name in "${file_names[@]}"; do
     output_path=$(echo "$directory$file_name" | sed "s/_metadata\.csv/.h5/")
