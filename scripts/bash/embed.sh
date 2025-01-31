d1041b22707a2031fa4bc6428a73c5da0d72ed92
Philippa Richter <pips.richter@gmail.com>
2025-01-31 00:17:15 -0800

    another try

--- scripts/bash/embed.sh
+++ scripts/bash/embed.sh
@@ -8,10 +8,18 @@ MEM=500GB
 TIME=24:00:00
 
 directory="../data/model_organisms/"
-# directory="../data/"
-
 file_names=("gtdb_ecol_metadata.csv" "gtdb_bsub_metadata.csv" "gtdb_mtub_metadata.csv" "gtdb_afis_metadata.csv" "gtdb_paer_metadata.csv")
-# file_names=("test_metadata.csv" "train_metadata.csv" "val_metadata.csv")
+
+
+for file_name in "${file_names[@]}"; do
+    output_path=$(echo "$directory$file_name" | sed "s/_metadata\.csv/.h5/")
+    job_name="embed_$file_name"
+    cmd="python embed.py --input-path \"$directory$file_name\" --output-path \"$output_path\""
+    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --gres="$GRES" -o "$job_name.out" --job-name "$job_name" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
+done
+
+directory="../data/"
+file_names=("test_metadata.csv" "train_metadata.csv" "val_metadata.csv")
 
 for file_name in "${file_names[@]}"; do
     output_path=$(echo "$directory$file_name" | sed "s/_metadata\.csv/.h5/")
