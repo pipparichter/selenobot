243d44eab7e3e85a9a5af8fc08dfc41b312b112f
Philippa Richter <pips.richter@gmail.com>
2025-02-01 06:14:20 -0800

    oop

--- scripts/bash/train.sh
+++ scripts/bash/train.sh
@@ -18,5 +18,5 @@ output_dim=2
 for feature_type in "${feature_types[@]}"; do
     job_name="train_${feature_type}_${output_dim}"
     cmd="python train.py --feature-type \"$feature_type\" --epochs $epochs --output-dim $output_dim"
-    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --job-name "$job_name" --gres="$GRES" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
+    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --job-name "$job_name" -o "$job_name.out" --gres="$GRES" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
 done
