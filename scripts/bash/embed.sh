2d2047163c258e5a0518a5d0620d0b3f87732101
Philippa Richter <pips.richter@gmail.com>
2025-02-09 23:00:20 -0800

    ughghg

--- scripts/bash/embed.sh
+++ scripts/bash/embed.sh
@@ -11,19 +11,19 @@ directory="../data/model_organisms/"
 file_names=("metadata_ecol.csv" "metadata_bsub.csv" "metadata_mtub.csv" "metadata_afis.csv" "metadata_paer.csv")
 
 
-for file_name in "${file_names[@]}"; do
-    job_name="embed_$file_name"
-    cmd="python embed.py --input-path \"$directory$file_name\""
-    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --gres="$GRES" -o "$job_name.out" --job-name "$job_name" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
-done
-
-
-# directory="../data/"
-# # file_names=($(ls "$directory" | grep '^[0-9]\+c_metadata_.*\.csv$'))
-# file_names=("2c_metadata_test.csv")
-
 # for file_name in "${file_names[@]}"; do
 #     job_name="embed_$file_name"
 #     cmd="python embed.py --input-path \"$directory$file_name\""
 #     sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --gres="$GRES" -o "$job_name.out" --job-name "$job_name" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
 # done
+
+
+directory="../data/"
+# file_names=($(ls "$directory" | grep '^[0-9]\+c_metadata_.*\.csv$'))
+file_names=("2c_metadata_test.csv" "2c_metadata_train.csv" "2c_metadata_val.csv")
+
+for file_name in "${file_names[@]}"; do
+    job_name="embed_$file_name"
+    cmd="python embed.py --input-path \"$directory$file_name\""
+    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --gres="$GRES" -o "$job_name.out" --job-name "$job_name" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
+done
