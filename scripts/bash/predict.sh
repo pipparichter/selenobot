0a9c3be6e491bba4b34392d22f4cf2727afd4c40
Philippa Richter <pips.richter@gmail.com>
2025-02-11 13:07:24 -0800

    finally ready for prediction stuff

--- scripts/bash/predict.sh
+++ scripts/bash/predict.sh
@@ -14,10 +14,11 @@ feature_types=("plm_esm_cls" "plm_esm_gap" "plm_pt5" "len" "aa_1mer")
 #     done
 # done
 
+feature_types=("plm_esm_cls" "plm_esm_gap" "plm_pt5" "len" "aa_1mer")
 model_types=("2c" "3c_xl")
 
 for feature_type in "${feature_types[@]}"; do
-    for model_types in "${model_types[@]}"; do
+    for model_type in "${model_types[@]}"; do
         python predict.py --model-name "model_${model_type}_${feature_type}" --feature-type "$feature_type" --input-path ../data/${model_type}_val.h5 
         python predict.py --model-name "model_${model_type}_${feature_type}" --feature-type "$feature_type" --input-path ../data/${model_type}_train.h5 
         python predict.py --model-name "model_${model_type}_${feature_type}" --feature-type "$feature_type" --input-path ../data/${model_type}_test.h5 
