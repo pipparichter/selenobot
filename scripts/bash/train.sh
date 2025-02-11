98ab6ba638d510941103b762f0174a244724c778
Philippa Richter <pips.richter@gmail.com>
2025-02-11 08:44:29 -0800

    now

--- scripts/bash/train.sh
+++ scripts/bash/train.sh
@@ -31,3 +31,10 @@ for feature_type in "${feature_types[@]}"; do
     cmd="python train.py --train-data-path \"../data/${output_dim}c_train.h5\" --val-data-path \"../data/${output_dim}c_val.h5\" --feature-type \"$feature_type\" --epochs $epochs --output-dim $output_dim"
     sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --job-name "$job_name" -o "$job_name.out" --gres="$GRES" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
 done
+
+for feature_type in "${feature_types[@]}"; do
+    job_name="train_3c_xl_${feature_type}"
+    # Make sure to specify the model name so it's not overwritten. 
+    cmd="python train.py --model-name model_3c_xl_$feature_type --train-data-path \"../data/3c_xl_train.h5\" --val-data-path \"../data/3c_xl_val.h5\" --feature-type \"$feature_type\" --epochs $epochs --output-dim 3"
+    sbatch --mem="$MEM" --time="$TIME" --partition="$PARTITION" --job-name "$job_name" -o "$job_name.out" --gres="$GRES" --mail-user="$MAILUSER" --mail-type="$MAILTYPE" --wrap "$cmd"
+done
